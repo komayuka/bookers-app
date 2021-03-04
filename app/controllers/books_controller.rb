4aea9a42d047748488ce7a0e78ddb3be7766bfaa
komayuka <komayuka115@gmail.com>
2021-03-04 05:54:58 +0000

    [progress]1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,11 @@
 class BooksController < ApplicationController
 
   def show
+      @book = Book.find(params[:id])
   end
 
   def index
+      @books = Book.all
   end
 
   def new
@@ -14,6 +16,7 @@ class BooksController < ApplicationController
   def create
       book = Book.new(book_params)
       book.save
+      redirect_to book_path(book.id)
   end
 
   def edit
